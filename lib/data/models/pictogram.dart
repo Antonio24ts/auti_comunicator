5b304f2a0d1a87ad466c58659ab07bfae3de8892
Antonio admin <antonio.dev14@gmail.com>
2026-06-19 03:23:43 +0200

    Add pictogram model

---
+++ lib/data/models/pictogram.dart
@@ -0,0 +1,13 @@
+class Pictogram {
+  final String id;
+  final String text;
+  final String imagePath;
+  final String categoryId;
+
+  const Pictogram({
+    required this.id,
+    required this.text,
+    required this.imagePath,
+    required this.categoryId,
+  });
+}
