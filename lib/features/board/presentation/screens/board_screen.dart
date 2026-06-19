c536b853ff98cfd1a5da7591a0f0faf83f828c66
Antonio admin <antonio.dev14@gmail.com>
2026-06-19 03:26:56 +0200

    Add basic responsive grid

--- lib/features/board/presentation/screens/board_screen.dart
+++ lib/features/board/presentation/screens/board_screen.dart
@@ -6,6 +6,22 @@ import '../widgets/pictogram_card.dart';
 class BoardScreen extends StatelessWidget {
   const BoardScreen({super.key});
 
+  int _getCrossAxisCount(double width) {
+    if (width >= 1000) {
+      return 5;
+    }
+
+    if (width >= 700) {
+      return 4;
+    }
+
+    if (width >= 500) {
+      return 3;
+    }
+
+    return 2;
+  }
+
   @override
   Widget build(BuildContext context) {
     final pictograms = PictogramRepository().getDefaultPictograms();
@@ -14,27 +30,33 @@ class BoardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tablero'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.builder(
-          itemCount: pictograms.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            childAspectRatio: 1.2,
-          ),
-          itemBuilder: (context, index) {
-            final pictogram = pictograms[index];
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final crossAxisCount = _getCrossAxisCount(constraints.maxWidth);
 
-            return PictogramCard(
-              pictogram: pictogram,
-              onTap: () {
-                debugPrint('Pulsado: ${pictogram.text}');
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: GridView.builder(
+              itemCount: pictograms.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                childAspectRatio: 1.2,
+              ),
+              itemBuilder: (context, index) {
+                final pictogram = pictograms[index];
+
+                return PictogramCard(
+                  pictogram: pictogram,
+                  onTap: () {
+                    debugPrint('Pulsado: ${pictogram.text}');
+                  },
+                );
               },
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
